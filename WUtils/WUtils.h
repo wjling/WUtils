1e628c3bb61816b1e9953c8d54b63e614a1c6df9
wjling <wjlingOo@126.com>
2021-08-02 14:37:22 +0800

    Initial Commit

---
+++ WUtils/WUtils.h
@@ -0,0 +1,18 @@
+//
+//  WUtils.h
+//  WUtils
+//
+//  Created by wjling on 2021/8/2.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WUtils.
+FOUNDATION_EXPORT double WUtilsVersionNumber;
+
+//! Project version string for WUtils.
+FOUNDATION_EXPORT const unsigned char WUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WUtils/PublicHeader.h>
+
+
