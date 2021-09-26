f7a8cc457131bd1a881d8b59b41972cebe48271a
wjling <wjlingOo@126.com>
2021-09-26 18:03:56 +0800

    [FIX] Color range exceeds

--- WUtils/Sources/UIColor/UIColor+Init.swift
+++ WUtils/Sources/UIColor/UIColor+Init.swift
@@ -10,6 +10,6 @@ import UIKit
 
 public extension UIColor {
     convenience init(hex: Int32, alpha: CGFloat = 1.0) {
-        self.init(red: CGFloat(hex >> 16), green: CGFloat((hex >> 8) & 0xFF), blue: CGFloat(hex & 0xFF), alpha: alpha)
+        self.init(red: CGFloat(hex >> 16) / 255, green: CGFloat((hex >> 8) & 0xFF) / 255, blue: CGFloat(hex & 0xFF) / 255, alpha: alpha)
     }
 }
